f1e18208744b86bd002ba3cf14f3c639245c2670
aleksandr <sanuyOKmaklaren0@gmail.com>
2023-10-26 04:43:04 +0500

    добавление дз

--- 01-variables/homework/index.js
+++ 01-variables/homework/index.js
@@ -1,1 +1,14 @@
-let aboba = 1338
+6 > 4 && 10 < 5 // false
+(30 + 4) > (7 ** 2) // false
+null && true //false
+!('hi' + 'hi') && true //false
+!('hi' + 'hi') && false //false
+
+'Привет' + 'Друг' == !null //Сравнение без учета типа данных
+'Привет' + 'Друг' === !null // С учетом
+
+const ten = 10; 
+const hundred = 100;
+const thousand = 1000;
+
+console.log((ten * ten + hundred) * hundred >= thousand * ten ) //true
