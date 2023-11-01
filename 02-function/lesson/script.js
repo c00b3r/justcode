74fbaa722ff3796d71a7fe7909241a08eb323649
aleksandr <sanuyOKmaklaren0@gmail.com>
2023-11-01 22:21:07 +0500

    fix soution for second task

--- 02-function/lesson/script.js
+++ 02-function/lesson/script.js
@@ -9,14 +9,18 @@ if (name == "Aleksandr") {
 }
 
 // 2 задание
-let health = 70;
+let health = 123;
 
-if (health >= 100) {
+if (health == 100) {
   console.log("Вы здоровы");
-} else if (health <= 75) {
+} else if (health < 100 && health >= 75) {
   console.log("Не помешало бы подлечиться");
-} else if (health <= 50) {
+} else if (health < 75 && health >= 50) {
+  console.log("Срочно надо подлечиться, уровень здоровья слишком низкий");
+} else if (health < 50 && health > 0) {
   console.log("Срочно надо подлечиться, уровень здоровья слишком низкий");
 } else if (health == 0) {
   console.log("Вы мертвы");
-} 
+} else {
+  console.log(`Ваш уровень здоровья ${health}`);
+}
