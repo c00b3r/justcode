90682c9e966a96b731b2888dd2edd35f02db9d4c
aleksandr <sanuyOKmaklaren0@gmail.com>
2023-11-04 15:13:24 +0500

    add first lesson task

---
+++ 03-cycle/lesson/script.js
@@ -0,0 +1,9 @@
+// 1
+// Написать цикл, который выводит все числа от 1 до 10.  - while do
+
+let counter = 1;
+while (counter >= 1 && counter <= 10) {
+    console.log(counter);
+    counter += 1
+  
+}
